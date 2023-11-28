c05981c7731ea843b0ffafecbd1cd965423d0453
Orphee <orphee.lonie@gmail.com>
2023-11-28 22:49:58 +0100

    [exo1] add part2

--- 2022/exo1/main.cpp
+++ 2022/exo1/main.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
 #include <fstream>
 
+void insert(int *maxs, int val)
+{
+    int i;
+    int next;
+    for (i = 0; i < 3; i++)
+        if (val > maxs[i])
+            std::swap(maxs[i], val);
+
+    for (; i < 3; i++)
+        std::swap(maxs[i], val);
+}
+
 int main(int argc, char** argv)
 {
     std::ifstream infile("input");
     std::string line;
 
-    int max = 0;
+    int max[3] = {0, 0, 0};
     int count = 0;
     while(getline(infile, line))
     {
@@ -14,12 +26,18 @@ int main(int argc, char** argv)
             count += std::atoi(line.c_str());
         else
         {
-            if (max < count)
-                max = count;
+            insert(max, count);
             count = 0;
         }
     }
-    std::cout << max << "\n";
+
+    int sum = 0;
+    for (int i = 0; i < 3; i++)
+    {
+        sum += max[i];
+        std::cout << max[i] << ", ";
+    }
+    std::cout << sum << "\n";
 
     return 0;
 }
