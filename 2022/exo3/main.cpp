974f7c0de353b10338cd529037b3155e2cbd8441
Orphee <orphee.lonie@gmail.com>
2023-11-29 00:03:21 +0100

    [exo3] add part1

--- 2022/exo3/main.cpp
+++ 2022/exo3/main.cpp
@@ -1,11 +1,39 @@
 #include <iostream>
 #include <fstream>
 
-void part1(std::string line, std::string &output)
+int count_score(char c)
 {
+    if (c <= 'z' && c >= 'a')
+        return c - 'a' + 1;
+    return c - 'A' + 27;
 }
 
-void part2(std::string line, std::string &output)
+void part1(std::string line, int &output)
+{
+    int length = line.length() / 2;
+    int map1[255] = {0};
+    int map2[255] = {0};
+    for (int i = 0; i < length; i++)
+    {
+        char c1 = line[i], c2 = line[i + length];
+        map1[c1] = 1;
+        map2[c2] = 1;
+
+        if (map2[c1])
+        {
+            output += count_score(c1);
+            return;
+        }
+
+        if (map1[c2])
+        {
+            output += count_score(c2);
+            return;
+        }
+    }
+}
+
+void part2(std::string line, int &output)
 {
 }
 
@@ -21,8 +49,8 @@ int main(int argc, char** argv)
 
     std::string line;
 
-    std::string output_part1 = 0;
-    std::string output_part2 = 0;
+    int output_part1 = 0;
+    int output_part2 = 0;
     while (getline(infile, line))
     {
         part1(line, output_part1);
