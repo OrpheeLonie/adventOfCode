ca7d53c893ca91eee1f42b5310e3b93704298986
Orphee <orphee.lonie@gmail.com>
2023-12-02 01:17:25 +0100

    [2023/exo1] correct some bugs

--- 2023/exo1/src/main.rs
+++ 2023/exo1/src/main.rs
@@ -43,34 +43,34 @@ fn parse_line(line: &str) -> u32 {
         "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
     ];
 
-    let mut first_index = 100000;
-    let mut first:u32 = 0;
+    let mut first_index = 100;
+    let mut first: u32 = 0;
     let mut last_index = 0;
-    let mut last:u32 = 0;
+    let mut last: u32 = 0;
 
     for i in 0..9 {
         let left_index = line.find((b'1' + i) as char);
         if left_index != None && left_index.unwrap() < first_index {
             first_index = left_index.unwrap();
-            first = i as u32;
+            first = (i + 1) as u32;
         }
 
         let left_index = line.find(l1[i as usize]);
         if left_index != None && left_index.unwrap() < first_index {
             first_index = left_index.unwrap();
-            first = i as u32;
+            first = (i + 1) as u32;
         }
 
         let right_index = line.rfind((b'1' + i) as char);
         if right_index != None && right_index.unwrap() > last_index {
             last_index = right_index.unwrap();
-            last = i as u32;
+            last = (i + 1) as u32;
         }
 
         let right_index = line.rfind(l1[i as usize]);
         if right_index != None && right_index.unwrap() > last_index {
             last_index = right_index.unwrap();
-            last = i as u32;
+            last = (i + 1) as u32;
         }
     }
     first * 10 + last
